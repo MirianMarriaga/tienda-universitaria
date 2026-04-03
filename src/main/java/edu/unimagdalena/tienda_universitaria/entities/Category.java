7645952e8fc212311944098575d3a45c1cf025f1
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-04-02 19:07:44 -0500

    feat: organizar la entidad category

--- src/main/java/edu/unimagdalena/tienda_universitaria/entities/Category.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/entities/Category.java
@@ -1,4 +1,30 @@
 package edu.unimagdalena.tienda_universitaria.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    @Builder.Default
+    private Set<Product> products = new HashSet<>();
 }
