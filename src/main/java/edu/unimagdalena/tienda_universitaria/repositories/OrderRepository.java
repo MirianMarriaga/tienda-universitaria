002080a98eb8159f26f7c0788083bfb27afbc002
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-04-03 15:04:21 -0500

    feat: Agregar query methods minimos

--- src/main/java/edu/unimagdalena/tienda_universitaria/repositories/OrderRepository.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/repositories/OrderRepository.java
@@ -1,4 +1,9 @@
 package edu.unimagdalena.tienda_universitaria.repositories;
 
+import edu.unimagdalena.tienda_universitaria.entities.Order;
+
+import java.util.List;
+
 public interface OrderRepository {
+    List<Order> findByCustomerId(Long customerId);
 }
--- src/main/java/edu/unimagdalena/tienda_universitaria/repositories/ProductRepository.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/repositories/ProductRepository.java
@@ -1,4 +1,15 @@
 package edu.unimagdalena.tienda_universitaria.repositories;
 
-public interface ProductRepository {
+import edu.unimagdalena.tienda_universitaria.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findBySku(String sku);
+
+    List<Product> findByCategoryIdAndActiveTrue(Long categoryId);
+
+    List<Product> findByInventoryAvailableStockLessThan(Integer stock);
 }
