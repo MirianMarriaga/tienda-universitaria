b233a06676f11856a75c4f043fd2f369e4d9e54d
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-04-03 15:27:15 -0500

    feat:Agregar query methods a customerRepository

--- src/main/java/edu/unimagdalena/tienda_universitaria/repositories/CustomerRepository.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/repositories/CustomerRepository.java
@@ -1,4 +1,16 @@
 package edu.unimagdalena.tienda_universitaria.repositories;
 
-public interface CustomerRepository {
+import edu.unimagdalena.tienda_universitaria.entities.Customer;
+import edu.unimagdalena.tienda_universitaria.entities.enums.CustomerStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+
+    List<Customer> findByStatus(CustomerStatus status);
+
+    Optional<Customer> findByIdentificationNumber(Long identificationNumber);
 }
