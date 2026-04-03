a17974c4254da2f49733de5cae2cafa4c8c9e23e
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-04-02 20:39:43 -0500

    feat:organizar la entidad orderStatusHistory

--- src/main/java/edu/unimagdalena/tienda_universitaria/entities/OrderItem.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/entities/OrderItem.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "orderItems")
+@Table(name = "order_items")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
--- src/main/java/edu/unimagdalena/tienda_universitaria/entities/OrderStatusHistory.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/entities/OrderStatusHistory.java
@@ -1,4 +1,30 @@
 package edu.unimagdalena.tienda_universitaria.entities;
 
+import edu.unimagdalena.tienda_universitaria.entities.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "order_status_histories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+
 public class OrderStatusHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(name = "changed_at", nullable = false)
+    private OrderStatus changedAAt;
 }
