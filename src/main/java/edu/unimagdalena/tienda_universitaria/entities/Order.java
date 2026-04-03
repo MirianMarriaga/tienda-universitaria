d64af6503478054adda9e43c2fab22ffc2bce11b
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-04-02 19:50:06 -0500

    organizar la entidad order

--- src/main/java/edu/unimagdalena/tienda_universitaria/entities/Order.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/entities/Order.java
@@ -1,4 +1,43 @@
 package edu.unimagdalena.tienda_universitaria.entities;
 
+import edu.unimagdalena.tienda_universitaria.entities.enums.CustomerStatus;
+import edu.unimagdalena.tienda_universitaria.entities.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    private BigDecimal total;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
 }
