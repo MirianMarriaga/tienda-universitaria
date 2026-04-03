9f5fdc8e7ee5b09158212c24f7f4761a0037b23b
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-04-02 19:30:21 -0500

    organizar entidad de inventory

--- src/main/java/edu/unimagdalena/tienda_universitaria/entities/Inventory.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/entities/Inventory.java
@@ -1,4 +1,28 @@
 package edu.unimagdalena.tienda_universitaria.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+
 public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name= "stock_available", nullable = false)
+    private String stockAvailable;
+
+    @Column(name= "minimum_stack", nullable = false)
+    private String minimumStack;
 }
