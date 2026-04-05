89ec4d0d40524fcfa46b90e655192ae6ae5a6ac4
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-04-05 11:40:36 -0500

    Agregar ProductService

--- src/main/java/edu/unimagdalena/tienda_universitaria/services/ProductService.java
+++ src/main/java/edu/unimagdalena/tienda_universitaria/services/ProductService.java
@@ -1,4 +1,17 @@
 package edu.unimagdalena.tienda_universitaria.services;
 
+import edu.unimagdalena.tienda_universitaria.api.dto.ProductDtos.*;
+
+import java.util.List;
+
 public interface ProductService {
+    ProductResponse create(ProductCreateRequest req);
+    ProductResponse get(Long id);
+    List<ProductResponse> list();
+    ProductResponse update(Long id, ProductUpdateRequest req);
+    void deactivate(Long id);
+
+    ProductResponse getProductBySku(String sku);
+    List<ProductResponse> getActiveProductsByCategory(Long categoryId);
+    List<LowStockProductResponse> getLowStockProducts();
 }
